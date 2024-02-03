2bcf5a498e84f8fbb9dae7e87b2a8b8160cda967
Juan Jose Ruggeri <jjoseruggeri@gmail.com>
2024-02-03 17:48:35 -0300

    Se agregan nuevos modelos DTO

---
+++ Models/ClientDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HomeBankingMindHub.Models
+{
+    public class ClientDto
+    {
+        [JsonIgnore]
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public ICollection<AccountDto> Accounts { get; set; }
+    }
+}
